95683e68f9b2eb639550b83dc98709d08cf107de
Sebastian Nevado <sebastian.nevado@devitech.com.co>
2024-05-06 18:02:20 -0500

    new changes

--- productly/settings.py
+++ productly/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.forms', # Add this line
     'productos.apps.ProductosConfig',
     
 ]
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'productly.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- productos/views.py
+++ productos/views.py
@@ -1,18 +1,44 @@
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+
+from productos.forms import ProductoForm
 from .models import Producto 
 
 # Create your views here.
 def index(request):
-  productos = Producto.objects.all().order_by('-id').values()
-  productos = list(productos)
-  
-  # productosPuntaje = Producto.objects.filter(puntaje=3)
-  # productosGet = Producto.objects.get(id=1)
+  productos = Producto.objects.all().order_by('-id')
   data = {
     'productos': productos,
-    'count': len(productos),
     'title': 'Listado de productos'
   }
-  print("Data: ", data)
-  return JsonResponse(data, safe=False)
+  return render(request,'index.html',context=data)
+
+def formulario(request):
+  form = ProductoForm()
+  context = {
+    'form': form
+    
+  }
+  if request.method == 'POST':
+    form = ProductoForm(request.POST)
+    if form.is_valid():
+      form.save()
+      context['mensaje'] = 'Producto guardado correctamente'
+      return HttpResponseRedirect('/productos')
+    else:
+      context['mensaje'] = 'Error al guardar el producto'
+  return render(request,'producto_form.html',context)
+  
+  
+
+def detalle(request, producto_id):
+  try:
+    # producto = get_object_or_404(Producto, pk=producto_id)
+    producto = Producto.objects.get(id=producto_id)
+    data = {
+      'producto': producto,
+      'title': producto.nombre
+    }
+    return render(request,'detalle.html',context=data)
+  except Producto.DoesNotExist:
+    return render (request,'404.html')
